1aea14f24a0104a645ad34d912366ff7a0aa0fe8
João Augusto Lissoni Lanjoni <joaoaugustolanjoni@gmail.com>
2024-09-29 19:02:35 -0300

    feat: add sample click button to presentation
    - add this simple button to prepare for presentation

--- src/com/example/views/landing.cljs
+++ src/com/example/views/landing.cljs
@@ -1,11 +1,18 @@
 (ns com.example.views.landing
   (:require
-   [helix.core :refer [$ defnc <>]]
+   [helix.core :refer [defnc]]
    [helix.dom :as d]
-   [helix.hooks :as hh]
-   [refx.alpha :as refx]))
+   [helix.hooks :as hh]))
 
 (defnc landing []
-  (d/div
-    {:class "bg-gray-100"}
-    "Hello, World!"))
+  (let [[count set-count] (hh/use-state 0)]
+    (d/div
+     {:class "bg-gray-100 p-4 text-center"}
+     (d/h1
+      {:class "text-xl mb-4"}
+      "Click Count: " count)
+     (d/button
+      {:class "bg-blue-500 text-white px-4 py-2 rounded"
+       :on-click #(set-count (inc count))}
+      "Click Me!"))))
+
